d376b7755997506ba03336a5fa075de374b5e398
Sean Oughton <seanoughton@gmail.com>
2018-03-16 21:59:55 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = {title: "The Room", release_date: 2003 }) #(args = {__})
+def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990 }) #(args = {__})
   Movie.create do |m|
     __
   end
