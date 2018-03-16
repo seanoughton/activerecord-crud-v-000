8782b7a5e96fab7a609b3f1b942d06f2c07fa04b
Sean Oughton <seanoughton@gmail.com>
2018-03-16 21:39:45 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316201742) do
+ActiveRecord::Schema.define(version: 20180316213839) do
 
   create_table "movies", force: :cascade do |t|
-    t.string "title"
+    t.string   "title"
+    t.datetime "release_date"
+    t.string   "director"
+    t.string   "lead"
+    t.boolean  "in_theaters"
   end
 
 end
