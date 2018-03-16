ac036f23f499fa92a329e1e21d6941feb9a00720
Sean Oughton <seanoughton@gmail.com>
2018-03-16 21:38:39 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
   __
 end
@@ -21,12 +21,19 @@ def can_be_created_with_a_hash_of_attributes
   }
   movie = __
 end
-
+=begin
 def can_be_created_in_a_block(args = {__})
   Movie.create do |m|
     __
   end
 end
+=end
+def can_be_created_in_a_block(args = {})
+  Movie.create do |m|
+    __
+  end
+end
+
 
 def can_get_the_first_item_in_the_database
   __
@@ -52,7 +59,7 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
   __
 end
@@ -92,11 +99,3 @@ def can_destroy_all_items_at_once
   end
   __
 end
-
-
-
-
-
-
-
-
