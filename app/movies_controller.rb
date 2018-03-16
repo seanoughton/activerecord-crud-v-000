3ea2dc1a74d5e707ed0b934b5b023adc094de053
Sean Oughton <seanoughton@gmail.com>
2018-03-16 22:01:21 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,6 +25,7 @@ end
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990 }) #(args = {__})
   Movie.create do |m|
     m.title = args[:title]
+    m.release_date = args[:release_date]
   end
 end
 
