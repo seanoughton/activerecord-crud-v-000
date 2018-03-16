09143c98bf4eb62a645e183008c6bef6bd5c38ec
Sean Oughton <seanoughton@gmail.com>
2018-03-16 22:27:04 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -58,9 +58,6 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by
   # release date descending
-  #
-  #Client.where("orders_count = ?", params[:orders])
- #Client.order(created_at: :desc)
   Movie.where("release_date > ?",2002).reverse_order
 
 end
